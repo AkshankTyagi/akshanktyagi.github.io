207d2ccfad983fc75536c8cf722d208179cb6d5a
AkshankTyagi <157580632+AkshankTyagi@users.noreply.github.com>
2025-10-17 10:55:51 +0530

    modified the Homepage

---
+++ script.js
@@ -0,0 +1,9 @@
+{/* <script>
+document.querySelectorAll(".toggle-btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const description = button.parentElement.nextElementSibling;
+    description.classList.toggle("show");
+    button.classList.toggle("active");
+  });
+});
+</script> */}
